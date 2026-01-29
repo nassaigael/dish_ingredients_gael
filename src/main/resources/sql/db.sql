d7babdcf8e4b9e8e87bb694c68b6112b1ea21482
nassaigael <hei.nassai@gmail.com>
2026-01-29 11:20:00 +0300

    chore(db): reinitialize database with proper ownership and privileges
    - Drop and recreate database with dedicated owner user
    - Set schema ownership to database manager
    - Grant default privileges on tables and sequences to manager
    - Assign full schema privileges to database manager

--- src/main/resources/sql/db.sql
+++ src/main/resources/sql/db.sql
@@ -1,5 +1,14 @@
-CREATE DATABASE "mini_dish_db";
+DROP DATABASE mini_dish_db;
+DROP USER mini_dish_db_manager;
 
-CREATE USER "mini_dish_db_manager" WITH PASSWORD '123456';
+CREATE USER mini_dish_db_manager WITH PASSWORD '123456';
+CREATE DATABASE mini_dish_db OWNER mini_dish_db_manager;
 
-GRANT ALL PRIVILEGES ON DATABASE "mini_dish_db" TO "mini_dish_db_manager";
+\c mini_dish_db postgres
+
+ALTER SCHEMA public OWNER TO mini_dish_db_manager;
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO mini_dish_db_manager;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO mini_dish_db_manager;
+
+GRANT ALL PRIVILEGES ON SCHEMA public TO mini_dish_db_manager;
